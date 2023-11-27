c656aaca32fa9a1cb5573d6f3c269d79f20b1279
DanniDevv <dante.rodriguez.c@tecsup.edu.pe>
2023-11-27 16:22:11 -0500

    Multiplataforma 12

---
+++ laboratorio04/lib/app/view/components/shape.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Shape extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset(
+        'assets/images/Vector.png',
+        width: 141,
+        height: 129,
+      ),
+    );
+  }
+}
